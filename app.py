8feb354554b3f8393de2afd9355b742a2e4fac71
apePetrus <shiveringveteran@proton.me>
2024-09-28 10:14:12 -0300

    added the option to mark as to-do when task is marked as done, needs to implement viewing done or to-do only

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 # Todo list app made in Flask
-
 from flask import Flask, render_template, request, redirect, url_for
 
+
 app = Flask(__name__)  # new flask instance setting the root of the app
+
 
 class Tasks:
     def __init__(self, task, status):
         self.task = task
         self.status = status
 
+
 task_list = []
+
 
 @app.route('/')
 def index():
     return render_template('index.html', task=task_list)
+
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -22,12 +26,26 @@ def add():
     task_list.append(new_task)
     return redirect(url_for('index'))
 
+
 @app.route('/delete/<int:index>')
 def delete(index):
     del task_list[index]
     return redirect(url_for('index'))
 
+
 @app.route('/done/<int:index>')
 def done(index):
     task_list[index].status = 'Done'
     return redirect(url_for('index'))
+
+
+@app.route('/todo/<int:index>')
+def todo(index):
+    task_list[index].status = 'To-do'
+    return redirect(url_for('index'))
+
+
+# Show only tasks marked as done
+# @app.route('/doneonly')
+# def doneonly():
+#     return 
